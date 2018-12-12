6227e473a82ec8e1f9785088a5f3cc4266f81028
wgeorge1990 <williameverettgeorge@icloud.com>
2018-12-12 08:58:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution in this file
+
+function findMatching(list, name){
+  return list.filter(function (driverName) {return driverName.toLowerCase()===name.toLowerCase()})
+}
+
+function fuzzyMatch(list, incompleteName){
+  nameLength = incompleteName.length
+  return list.filter(function (driverName) {return driverName.slice(0, nameLength) === incompleteName})
+}
+
+function matchName(list, name){
+  return list.filter(function (driver) {return driver.name.toLowerCase()=== name.toLowerCase()})
+
+}
